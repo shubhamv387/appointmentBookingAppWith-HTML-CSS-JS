1353d3e9ba94dd753350f12a3162aba5f783d276
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-12 10:18:52 +0530

    added checks for update button

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -63,6 +63,8 @@ function showUserOnScreen(userObj) {
     userObj.email +
     "<br> <span>Phone: </span>" +
     userObj.phone +
+    "<br> <span>UserId: </span>" +
+    userObj.id +
     "<br></div>";
 
   //creare edit btn
@@ -88,6 +90,7 @@ function showUserOnScreen(userObj) {
     document.getElementById("submitbtn").style.display = "none";
     document.getElementById("updatebtn").style.display = "block";
 
+    const msg = document.querySelector(".msg");
     let updateName = document.getElementById("name");
     let updateEmail = document.getElementById("email");
     let updatePhone = document.getElementById("phone");
@@ -96,96 +99,98 @@ function showUserOnScreen(userObj) {
     updatePhone.value = userObj.phone;
     updateName.focus();
 
-    // localStorage.removeItem(userObj.email);
-
     //While adding event listeners to the "EDIT" button, I encountered an issue. Clicking the "EDIT" button multiple times registers multiple event listeners on the "UPDATE" button. This leads to errors when clicking the "UPDATE" button, as all the registered event listeners are triggered, causing data changes for each "EDIT" button clicked.
 
     //and then I added (.onclick) eventHandeler and this problem gone.
     document.getElementById("updatebtn").onclick = () => {
-      let newUserObj = {
-        name: updateName.value,
-        email: updateEmail.value,
-        phone: updatePhone.value,
-      };
+      if (
+        updateName.value === "" ||
+        updateEmail.value === "" ||
+        updatePhone.value === ""
+      ) {
+        msg.classList.add("error");
+        msg.innerHTML = "Please enter all fields";
 
-      if (newUserObj.email === userObj.email) {
-        axios
-          .put(`http://localhost:5000/edit-user/${userObj.id}`, newUserObj)
-          .then((response) => {
-            // console.log(response.data);
-            userObj = { ...response.data };
-            user.innerHTML =
-              "<div><span>Name: </span>" +
-              userObj.name +
-              "<br> <span>Email: </span>" +
-              userObj.email +
-              "<br> <span>Phone: </span>" +
-              userObj.phone +
-              "<br></div>";
-
-            let editBtn = document.createElement("button");
-            editBtn.className = "editBtn";
-            editBtn.appendChild(document.createTextNode("EDIT"));
-
-            user.appendChild(editBtn);
-
-            let delBtn = document.createElement("button");
-            delBtn.className = "delBtn";
-            delBtn.appendChild(document.createTextNode("DELETE"));
-
-            user.appendChild(delBtn);
-            users.appendChild(user);
-
-            editBtn.addEventListener("click", editUser);
-            delBtn.addEventListener("click", deleteUser);
-
-            updateName.value = "";
-            updateEmail.value = "";
-            updatePhone.value = "";
-
-            // changing back to the original display styles of both btns
-            document.getElementById("submitbtn").style.display = "block";
-            document.getElementById("updatebtn").style.display = "none";
-          })
-          .catch((err) => console.log(err.message));
+        // Remove error after 3 seconds
+        setTimeout(() => {
+          msg.classList.remove("error");
+          msg.innerHTML = "";
+        }, 3000);
       } else {
-        // Checking for the duplicate email Address on updating
-        axios
-          .get("http://localhost:5000/")
-          .then((response) => {
-            if (!isEmailAlreadyExists(response, newUserObj)) {
-              axios
-                .put(
-                  `http://localhost:5000/edit-user/${userObj.id}`,
-                  newUserObj
-                )
-                .then((response) => {
-                  userObj = { ...response.data };
-                  user.firstElementChild.innerHTML =
-                    "<div><span>Name: </span>" +
-                    userObj.name +
-                    "<br> <span>Email: </span>" +
-                    userObj.email +
-                    "<br> <span>Phone: </span>" +
-                    userObj.phone +
-                    "<br></div>";
+        let newUserObj = {
+          name: updateName.value,
+          email: updateEmail.value,
+          phone: updatePhone.value,
+        };
 
-                  updateName.value = "";
-                  updateEmail.value = "";
-                  updatePhone.value = "";
+        if (newUserObj.email === userObj.email) {
+          axios
+            .put(`http://localhost:5000/edit-user/${userObj.id}`, newUserObj)
+            .then((response) => {
+              // console.log(response.data);
+              userObj = response.data;
+              user.firstElementChild.innerHTML =
+                "<div><span>Name: </span>" +
+                userObj.name +
+                "<br> <span>Email: </span>" +
+                userObj.email +
+                "<br> <span>Phone: </span>" +
+                userObj.phone +
+                "<br> <span>UserId: </span>" +
+                userObj.id +
+                "<br></div>";
 
-                  // changing back to the original display styles of both btns
-                  document.getElementById("submitbtn").style.display = "block";
-                  document.getElementById("updatebtn").style.display = "none";
-                })
-                .catch((err) => console.log(err.message));
-            }
-          })
-          .catch((err) => console.log(err));
+              updateName.value = "";
+              updateEmail.value = "";
+              updatePhone.value = "";
+
+              // changing back to the original display styles of both btns
+              document.getElementById("submitbtn").style.display = "block";
+              document.getElementById("updatebtn").style.display = "none";
+            })
+            .catch((err) => console.log(err.message));
+        } else {
+          // Checking for the duplicate email Address on updating
+          axios
+            .get("http://localhost:5000/")
+            .then((response) => {
+              if (!isEmailAlreadyExists(response, newUserObj)) {
+                axios
+                  .put(
+                    `http://localhost:5000/edit-user/${userObj.id}`,
+                    newUserObj
+                  )
+                  .then((response) => {
+                    userObj = response.data;
+                    user.firstElementChild.innerHTML =
+                      "<div><span>Name: </span>" +
+                      userObj.name +
+                      "<br> <span>Email: </span>" +
+                      userObj.email +
+                      "<br> <span>Phone: </span>" +
+                      userObj.phone +
+                      "<br> <span>UserId: </span>" +
+                      userObj.id +
+                      "<br></div>";
+
+                    updateName.value = "";
+                    updateEmail.value = "";
+                    updatePhone.value = "";
+
+                    // changing back to the original display styles of both btns
+                    document.getElementById("submitbtn").style.display =
+                      "block";
+                    document.getElementById("updatebtn").style.display = "none";
+                  })
+                  .catch((err) => console.log(err.message));
+              }
+            })
+            .catch((err) => console.log(err));
+        }
       }
     };
   }
-  //remove user details from browser list and localStorage.
+  //remove user details from browser list and Database.
   delBtn.addEventListener("click", deleteUser);
   function deleteUser() {
     const result = confirm("Are You Sure?");
